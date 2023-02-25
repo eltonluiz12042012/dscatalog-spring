397d70d39885d4ac0a129a8fc3fd64722bfa1b3f
Elton Luiz <eltonluiz12042012@gmail.com>
2023-02-25 08:12:39 -0300

    Backend project created

---
+++ backend/src/main/java/com/dscatalog/dscatalog/DscatalogProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.dscatalog.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogProjetoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dscatalog/dscatalog/DscatalogProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dscatalog.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
